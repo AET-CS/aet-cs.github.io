b94874944e5c9f1ceb8679e616fe10d89e7e5159
paderevski <75690676+paderevski@users.noreply.github.com>
2024-10-21 07:51:36 -0400

    CS 10 Arrays

---
+++ white/AP_shared/Unit_2/Array_Basics_Labs/ArrayBasics2.java
@@ -0,0 +1,71 @@
+public class ArrayBasics2 {
+
+	/**
+	 * NOTE: Use a for each loop for this problem.
+	 * 
+	 * input: an array of integers
+	 * output: a count of elements that are odd AND greater than 50
+	 *
+	 * countOddOver50 ( {6, 51, 90, 61, 12, 14} ) => 2
+	 */
+	public static int countOddOver50(int[] a) {
+
+		return 0;
+	}
+
+	/**
+	 * input: an array of integers
+	 * output: an array of integers with only positive elements
+	 *
+	 * removeNegatives ( {10, -11, 12, -13} ) => {10, 12}
+	 *
+	 * You will want to create a new array and add only the positive
+	 * elements from the input array to it.
+	 * Count the number of positives first to determine the size of
+	 * the new array.
+	 */
+
+	public static int[] removeNegatives(int[] a) {
+		
+		return null;
+	}
+
+	/**
+	 * count the number of consecutive equal elements
+	 * i.e. the number of values that are equal to the previous value.
+	 * Note: the 0th element has no previous element
+	 * 
+	 * @param ss : an array of Strings with positive length
+	 * @return : int representing the number of pairs as described above
+	 */
+	public static int countPairs(String[] ss) {
+		return 0;
+	}
+
+	/**
+	 * determine if there are duplicate elements in the array
+	 * 
+	 * @param nums : an array of integers with positive length
+	 * @return : true if no two integers in the array are equal, false otherwise
+	 */
+	public static boolean hasNoDuplicates(int[] nums) {
+		
+		return false;
+	}
+
+	/**
+	 * shift the values in the array k values to the right, wrapping around if they
+	 * go out the end
+	 * 
+	 * @param ns : an array of ints with positive length
+	 * @param k  : an int that is a valid index for ns
+	 * @return : the shifted array
+	 * 
+	 * rightShift ( {2, 4, 6, 8}, 2 ) => {6, 8, 2, 4}
+	 */
+
+	public static int[] rightShift(int[] ns, int k) {
+
+		return null;
+	}
+}

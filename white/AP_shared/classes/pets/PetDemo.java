f57f4ac04d87df052319f22b21556635b9a0f51d
Kavin Muralikrishnan <142550397+Advay17@users.noreply.github.com>
2024-12-04 10:26:18 -0500

    Update PetDemo.java

--- white/AP_shared/classes/pets/PetDemo.java
+++ white/AP_shared/classes/pets/PetDemo.java
@@ -1,7 +1,7 @@
 public class PetDemo {
 	public static void main(String[] args) {
 		Pet perry = new Dog("perry", 10, "doberman");
-		perry.makeSound();
+		System.out.println(perry.makeSound());
 		System.out.println(perry.getInfo());
 		((Dog)perry).fetch();
 	}
